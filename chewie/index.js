9324eeb03d94d3a183556b044631dad930399a22
Mats Grimsgaard <mats@mgv.no>
2020-08-25 10:46:21 +0200

    Refactor chewie

--- chewie/index.js
+++ chewie/index.js
@@ -1,66 +1,7 @@
 "use strict";
-const Papa = require("papaparse");
-const fs = require("fs");
 const klass = require("./klass");
 const han = require("./han");
 const luke = require("./luke");
-
-const mainDataIndex = require("../data/index.json");
-
-const MAX_HISTORY_YEARS = 5;
-
-const getConfig = (year) => {
-  process.stdout.write(`Laster konfigurasjon for ${year}... `);
-
-  const config = JSON.parse(fs.readFileSync(`data/config.json`, "utf-8"));
-  const years = Object.keys(config).sort().reverse();
-
-  const result = config[years.find((y) => y <= year)];
-
-  if (!result) {
-    console.log("\x1b[31m\x1b[1mFeil\x1b[0m");
-    console.log("Finner ingen konfigurasjon for dette året.");
-    process.exit();
-  }
-
-  console.log("\x1b[1mOK\x1b[0m");
-
-  return result;
-};
-
-const hasData = (year) => {
-  return fs.existsSync(`data/raw/g${year}.csv`);
-};
-
-const getData = (year) => {
-  process.stdout.write(`Laster data for ${year}... `);
-
-  const localFile = fs.readFileSync(`data/raw/g${year}.csv`, "utf-8");
-  const file = Papa.parse(localFile, { dynamicTyping: true });
-
-  if (file.errors.length) {
-    console.log("\x1b[31m\x1b[1mFeil\x1b[0m");
-    console.log(file.errors);
-    process.exit();
-  }
-
-  console.log("\x1b[1mOK\x1b[0m");
-
-  const hours = file.data.reduce((acc, row) => acc + row[23], 0);
-
-  console.log(
-    "\x1b[2m",
-    hours.toLocaleString().padStart(10, " "),
-    "timer\x1b[0m"
-  );
-  console.log(
-    "\x1b[2m",
-    file.data.length.toLocaleString().padStart(10, " "),
-    "kurs\x1b[0m"
-  );
-
-  return file.data;
-};
 
 console.log(`               __
               / _)
@@ -78,25 +19,10 @@ if (!year || !year.match(/^\d{4}$/)) {
 }
 
 async function main() {
-  const config = getConfig(year);
-  const data = getData(year);
+  const config = luke.getConfig(year);
+  const data = luke.getData(year);
   const municipalities = await klass.getData(year);
-
-  console.log(`Ser etter historiske data ${MAX_HISTORY_YEARS} år tilbake... `);
-
-  const dataHistory = {};
-  for (
-    let historyYear = year - 1;
-    historyYear > year - MAX_HISTORY_YEARS;
-    historyYear--
-  ) {
-    if (hasData(historyYear)) {
-      dataHistory[historyYear] = getData(historyYear);
-    } else {
-      console.log(`Finner ikke data for ${historyYear}. `);
-      break;
-    }
-  }
+  const dataHistory = luke.getDataHistory(year);
 
   process.stdout.write(`Analyserer... `);
 
@@ -160,22 +86,8 @@ async function main() {
   Object.keys(config.regions).map(makeStat);
   console.log("\x1b[1mOK\x1b[0m");
 
-  process.stdout.write(`Lagrer resultater... `);
-  const wstream = fs.createWriteStream(`data/${year}.json`);
-  wstream.write(JSON.stringify({ municipalities, reports }));
-  wstream.end();
-  console.log("\x1b[1mOK\x1b[0m");
-
-  // Append to main data index
-  process.stdout.write(`Legger til ${year} i indeks... `);
-  mainDataIndex.years = [...new Set([...mainDataIndex.years, year])]
-    .sort()
-    .reverse();
-
-  const istream = fs.createWriteStream(`data/index.json`);
-  istream.write(JSON.stringify(mainDataIndex));
-  istream.end();
-  console.log("\x1b[1mOK\x1b[0m");
+  // Save results
+  luke.useTheForce(year, { municipalities, reports });
 }
 
 main();
