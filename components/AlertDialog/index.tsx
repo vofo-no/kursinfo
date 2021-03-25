52d0e0fbc2f869f4e10d05919bd3e20bdac6260e
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2021-03-25 08:50:04 +0100

    Cleanup dead code

--- components/AlertDialog/index.tsx
+++ components/AlertDialog/index.tsx
@@ -22,10 +22,6 @@ const AlertDialog: FC<AlertDialogProps> = ({
     if (keyCode === 27) close();
   }, []);
 
-  const handleClick = useCallback((event) => {
-    if (r.current && !r.current.contains(event.target)) close();
-  }, []);
-
   useEffect(() => {
     window.addEventListener("keydown", handleUserKeyPress);
 
@@ -109,60 +105,6 @@ const AlertDialog: FC<AlertDialogProps> = ({
         }
       `}</style>
     </AriaModal>
-  );
-  return (
-    <>
-      <div className="backdrop" onClick={handleClick}>
-        <div
-          role="dialog"
-          aria-labelledby="alert-dialog-1-title"
-          aria-modal="true"
-          ref={r}
-        ></div>
-      </div>
-      <style jsx global>{`
-        body {
-          overflow: hidden;
-        }
-      `}</style>
-      <style jsx>{`
-        .backdrop {
-          position: fixed;
-          background-color: rgba(255, 255, 255, 0.3);
-          backdrop-filter: blur(10px);
-          overflow-y: auto;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        [role="dialog"] {
-          background: white;
-          border: 2px solid gray;
-          padding: 2rem;
-          position: relative;
-        }
-
-        h2 {
-          margin-right: 2rem;
-          margin-top: 0;
-          padding-top: 0;
-        }
-
-        .close {
-          padding: 0.5rem;
-          margin: 0.5rem;
-          cursor: pointer;
-          position: absolute;
-          top: 0;
-          right: 0;
-        }
-      `}</style>
-    </>
   );
 };
 
