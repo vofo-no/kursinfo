c3544e0f6671af7f09eae60b93f490005315fd72
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2021-03-25 07:57:09 +0100

    Refactor constants and add organization filter

--- components/Courses/constants.ts
+++ components/Courses/constants.ts
@@ -1,4 +1,13 @@
-export const ALL_COUNTIES_OPTION = ["hele-landet", "Hele landet"];
+import {
+  DEFAULT_COUNTY_PARAM,
+  DEFAULT_ORGANIZATION_PARAM,
+} from "../../lib/constants";
+
+export const ALL_COUNTIES_OPTION = [DEFAULT_COUNTY_PARAM, "Hele landet"];
+export const ALL_ORGANIZATIONS_OPTION = [
+  DEFAULT_ORGANIZATION_PARAM,
+  "Alle organisasjoner",
+];
 export const GROUPS = [
   "kurs",
   "lag",
@@ -7,3 +16,38 @@ export const GROUPS = [
   "studieplaner",
 ] as const;
 export type GroupType = typeof GROUPS[number];
+
+export function isDefaultCounty(val: string) {
+  return val === DEFAULT_COUNTY_PARAM;
+}
+
+export function isDefaultOrganization(val: string) {
+  return val === DEFAULT_ORGANIZATION_PARAM;
+}
+
+export interface ICoursesBaseProps {
+  county: string;
+  countyOptions: string[][];
+  organization: string;
+  organizationOptions: string[][];
+  year: string;
+  yearOptions: string[];
+}
+
+export interface CoursesParams {
+  year?: string;
+  county?: string;
+  organization?: string;
+  group?: GroupType;
+}
+
+export interface IAggregatedData {
+  id: string;
+  name: string;
+  plannedCourses?: number;
+  plannedHours?: number;
+  courses: number;
+  hours: number;
+  participants: number;
+  grant: number;
+}
