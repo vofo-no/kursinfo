90a5fdf0b159333daef46646b74f177a1dfb4fc0
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2021-06-24 11:55:10 +0200

    chore: fix eslint errors

--- pages/funkis/[year]/[county]/[organization]/[group]/index.tsx
+++ pages/funkis/[year]/[county]/[organization]/[group]/index.tsx
@@ -8,13 +8,11 @@ import getCoursesStaticPaths from "components/Courses/getStaticPaths";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { CoursesParams } from "types/courses";
 
-export const getStaticProps: GetStaticProps<
-  CoursesProps,
-  CoursesParams
-> = async ({ params }) => {
-  if (!params) throw new Error();
-  return getCoursesStaticData(params, TENANT);
-};
+export const getStaticProps: GetStaticProps<CoursesProps, CoursesParams> =
+  async ({ params }) => {
+    if (!params) throw new Error();
+    return getCoursesStaticData(params, TENANT);
+  };
 export const getStaticPaths: GetStaticPaths<CoursesParams> = async () => {
   return getCoursesStaticPaths(TENANT);
 };
