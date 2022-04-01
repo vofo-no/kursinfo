d191d7daaaa53c8a87be334a21258a06a0e43f2d
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-04-01 13:52:38 +0200

    feat: import polyfills conditionally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,29 @@
-import "@formatjs/intl-numberformat/polyfill-force";
-import "@formatjs/intl-numberformat/locale-data/nb";
-import "@formatjs/intl-datetimeformat/polyfill-force";
-import "@formatjs/intl-datetimeformat/add-all-tz";
-import "@formatjs/intl-datetimeformat/locale-data/nb";
 import "../styles/globals.css";
 
+import { shouldPolyfill as shouldPolyfillDateTimeFormat } from "@formatjs/intl-datetimeformat/should-polyfill";
+import { shouldPolyfill as shouldPolyfillNumberFormat } from "@formatjs/intl-numberformat/should-polyfill";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { IntlProvider } from "react-intl";
-import { resetIdCounter } from "react-tabs";
+
+function polyfill() {
+  if (shouldPolyfillNumberFormat("nb")) {
+    import("@formatjs/intl-numberformat/polyfill-force");
+    import("@formatjs/intl-numberformat/locale-data/nb");
+  }
+
+  if (shouldPolyfillDateTimeFormat("nb")) {
+    import("@formatjs/intl-datetimeformat/polyfill-force");
+    import("@formatjs/intl-datetimeformat/add-all-tz");
+    import("@formatjs/intl-datetimeformat/locale-data/nb");
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
   const { locale } = router;
-  resetIdCounter();
+  polyfill();
+
   return (
     <IntlProvider locale={locale || "nb"}>
       <Component {...pageProps} />
