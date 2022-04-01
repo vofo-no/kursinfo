72a15afe7a5969cc517ee0ba701eee0fb86b106d
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-04-01 13:49:45 +0200

    fix: use ui package in headers

--- components/Courses/customHeaders/KstudHeader.tsx
+++ components/Courses/customHeaders/KstudHeader.tsx
@@ -1,28 +1,23 @@
-import { Box } from "@vofo-no/design";
+import Container from "components/Containers/Container";
 import Image from "next/image";
 
 const KstudHeader = (): JSX.Element => (
-  <Box variant="light" boxShadow="small">
-    <Box
-      px={2}
-      pb={1}
-      pt={2}
-      backgroundColor="#f8f8f8"
-      style={{ borderBottom: "1px solid #e7e7e7" }}
-    >
-      <Box container>
-        <a href="https://www.k-stud.no/">
-          <Image
-            width={155}
-            height={48}
-            layout="intrinsic"
-            src="/KstudLogo.png"
-            alt="Kristelig studieforbund"
-          />
-        </a>
-      </Box>
-    </Box>
-  </Box>
+  <div className="bg-[#f8f8f8] border-b border-b-gray-300">
+    <Container noPadding>
+      <a
+        href="https://www.k-stud.no/"
+        className="inline-block mx-2 tablet:mx-6 mt-4 mb-2"
+      >
+        <Image
+          width={155}
+          height={48}
+          layout="intrinsic"
+          src="/KstudLogo.png"
+          alt="Kristelig studieforbund"
+        />
+      </a>
+    </Container>
+  </div>
 );
 
 export default KstudHeader;
--- components/Courses/customHeaders/MsfHeader.tsx
+++ components/Courses/customHeaders/MsfHeader.tsx
@@ -1,16 +1,11 @@
-import { Box } from "@vofo-no/design";
+import Container from "components/Containers/Container";
+import WhiteBox from "components/Containers/WhiteBox";
 import Image from "next/image";
 
 const MsfHeader = (): JSX.Element => (
-  <Box variant="light" boxShadow="small">
-    <Box
-      px={2}
-      pb={1}
-      pt={2}
-      backgroundColor="#fff3ea"
-      style={{ borderTop: "4px solid #e95b35" }}
-    >
-      <Box container>
+  <WhiteBox noPadding>
+    <div className="bg-[#fff3ea] border-t-4 border-t-[#e95b35] pt-4 pb-2 px-2 tablet:px-6">
+      <Container noPadding>
         <a href="https://www.musikkensstudieforbund.no/">
           <Image
             sizes="(max-width: 750px) 151px, 213px"
@@ -21,9 +16,9 @@ const MsfHeader = (): JSX.Element => (
             alt="Musikkens studieforbund"
           />
         </a>
-      </Box>
-    </Box>
-  </Box>
+      </Container>
+    </div>
+  </WhiteBox>
 );
 
 export default MsfHeader;
