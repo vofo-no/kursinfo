54d825072eb92be70d749afdc547a4c577673435
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-04-07 13:13:23 +0200

    feat: add report footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@vofo-no/ui";
 import Head from "next/head";
 import { FC } from "react";
 
@@ -43,6 +44,25 @@ const Layout: FC<LayoutProps> = ({ title, children, header = false }) => {
     <>
       {LayoutHead}
       <main style={{ counterReset: "tables figures" }}>{children}</main>
+      <footer className="bg-gray-200 p-2 py-8">
+        <div className="max-w-fit mx-auto print:m-0 print:w-full print:max-w-none flex flex-wrap gap-4">
+          <div className="prose prose-sm">
+            <p>
+              Voksenopplæringsforbundet (Vofo) er studieforbundenes
+              interesseorganisasjon, nasjonalt og regionalt. Vårt hovedmål er å
+              styrke studieforbundenes rammevilkår i arbeidet med å gi voksne
+              læringsmuligheter.
+            </p>
+            <p>
+              Du kan lære mer om studieforbund og bli bedre kjent med oss på{" "}
+              <a href="https://www.vofo.no">vofo.no</a>.
+            </p>
+          </div>
+          <div>
+            <Logo className="w-40" />
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
