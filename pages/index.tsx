117f1ba2d88bed5aa75495afa6e2fb966a46736b
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2023-05-12 10:38:20 +0200

    feat: add illustration on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import WhiteBox from "components/Containers/WhiteBox";
 import GridLinks from "components/GridLinks";
 import { GetStaticProps } from "next";
+import Image from "next/image";
 import { FC } from "react";
 
 import Layout from "../components/Layout";
 import dataIndex from "../data/index.json";
+import illustration from "../public/undraw_upgrade_re_gano.svg";
 
 interface IndexProps {
   years: Array<string>;
@@ -24,19 +26,27 @@ const Index: FC<IndexProps> = ({ years }) => {
       <WhiteBox>
         <div className="prose">
           <h1 className="text-4xl mb-0">Statistikk</h1>
-          <p className="lead">
-            Her finner du Vofos nasjonale og regionale statistikkrapporter om
-            studieforbundenes kursvirksomhet.
-          </p>
-          <p>
-            Rapportene viser omfanget og utviklingen av tilskuddsberettiget
-            kursvirksomhet i ulike studieforbund og fylker. Kilde til rapportene
-            er Statistisk sentralbyrå (SSB) sin offisielle statistikk over{" "}
-            <a href="https://ssb.no/voppl">
-              studieforbundenes opplæringsvirksomhet
-            </a>
-            .
-          </p>
+        </div>
+        <div className="flex flex-col tablet:flex-row-reverse">
+          <div className="mt-6 tablet:mt-0">
+            <Image src={illustration} alt="" />
+          </div>
+          <div className="prose mt-6">
+            <p className="lead">
+              Her finner du Vofos nasjonale og regionale statistikkrapporter om
+              studieforbundenes kursvirksomhet.
+            </p>
+            <p>
+              Rapportene viser omfanget og utviklingen av tilskuddsberettiget
+              kursvirksomhet i ulike studieforbund og fylker. Kilde til
+              rapportene er Statistisk sentralbyrå (SSB) sin offisielle
+              statistikk over{" "}
+              <a href="https://ssb.no/voppl">
+                studieforbundenes opplæringsvirksomhet
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </WhiteBox>
       <GridLinks items={years.map((year) => [`/${year}`, year])} />
