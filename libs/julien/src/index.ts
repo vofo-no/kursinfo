6f534188fd2b87db656d2643697d75232f283c91
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2025-01-03 14:52:20 +0100

    feat: export ITenantData type

--- libs/julien/src/index.ts
+++ libs/julien/src/index.ts
@@ -6,3 +6,4 @@ export {
 export { isValidTarget, updateStatistics } from "./update-tenant-data";
 export { afterMaxLastYear } from "./helpers/after-max-last-year";
 export { getTenantId } from "./get-tenant-id";
+export type { ITenantData } from "./types";
