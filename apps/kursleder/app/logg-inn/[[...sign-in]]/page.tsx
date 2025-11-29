ab6b2d056c15f5fb1b6cfffae98a4185df9e680e
Mats Grimsgaard <mats@mgv.no>
2025-11-29 09:07:57 +0100

    chore: remove unused kursleder app

--- apps/kursleder/app/logg-inn/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="row-span-3">
-      <SignIn withSignUp />
-    </main>
-  );
-}
--- apps/kursleder/components/article.tsx
+++
@@ -1,7 +0,0 @@
-export function Article({ children }: React.PropsWithChildren) {
-  return (
-    <article className="prose mt-2 mb-12 w-full prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h1:font-normal prose-h1:first:mt-0 prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
-      {children}
-    </article>
-  );
-}
