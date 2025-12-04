df07557194723fd8e2b1394741960801e58c491a
Mats Grimsgaard <mats@mgv.no>
2025-12-04 09:31:15 +0100

    feat: reset datahub app

--- apps/datahub/app/(dashboard)/[scope]/kurslarere/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function TeachersLayout({ children }: PropsWithChildren) {
-  return <main>{children}</main>;
-}
--- apps/datahub/lib/intl.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = new Intl.DateTimeFormat("nb-NO", {
-  dateStyle: "medium",
-  timeStyle: "short",
-});
-
-export const numberFormat = new Intl.NumberFormat("nb-NO", {
-  maximumFractionDigits: 1,
-});
