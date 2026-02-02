2352897018616bea17e851833c907e4de42aa9b9
Mats Grimsgaard <mats@mgv.no>
2026-02-02 09:27:22 +0100

    feat: optimize static rendering

--- apps/statistikk/app/(studieforbund)/skt/route.ts
+++ apps/statistikk/app/(studieforbund)/skt/route.ts
@@ -5,9 +5,15 @@ import { StudieforbundParams } from "@/app/(studieforbund)/types";
 
 import { config } from "./config";
 
+export const dynamic = "force-dynamic";
+
+export async function generateStaticParams() {
+  return [];
+}
+
 export async function GET(
   request: NextRequest,
-  props: { params: Promise<Partial<StudieforbundParams>> }
+  props: { params: Promise<Partial<StudieforbundParams>> },
 ) {
   const params = await props.params;
   return redirectRoute(config.tenant, request.nextUrl.pathname, params);
